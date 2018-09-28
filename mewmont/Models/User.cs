fe4d01a72f84be9100bd434b3f2d38d42e7a955a
Pete <phuc.le@student.qut.edu.au>
2018-09-28 16:41:28 +1000

    create user properties

--- mewmont/Models/User.cs
+++ mewmont/Models/User.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Xamarin.Forms;
 
 namespace mewmont.Models
 {
-    class User
+    public class User
     {
+        private string username;
+        private string emailAddress;
+        private Image profilePicture;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string EmailAddress
+        {
+            get { return emailAddress; }
+            set { emailAddress = value; }
+        }
+
+        public Image ProfilePicture
+        {
+            get { return profilePicture; }
+            set { profilePicture = value; }
+        }
     }
 }
