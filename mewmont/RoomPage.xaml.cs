334a8a1fbfe2d4dca8127e02aee0574a7ca88082
Steven Harse <fukachiongakuka@gmail.com>
2018-09-28 19:55:23 +1000

    Implemented more events

--- mewmont/RoomPage.xaml.cs
+++ mewmont/RoomPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,16 @@ namespace mewmont
         private void MessageEntry_Unfocused(object sender, FocusEventArgs e)
         {
             OptionButtons.IsVisible = true;
+        }
+
+        async void ChatModeBtn_OnClick(object sender, EventArgs e)
+        {
+            var option = await DisplayActionSheet("Choose a chat mode", "Cancel", null, "Text-only", "Voice", "Camera & Voice");
+        }
+
+        async void LeaveRoom_Triggered(object sender, EventArgs e)
+        {
+            var answer = await DisplayAlert("Are you sure?", "Do you want to leave this room?", "Yes", "No");
         }
     }
 }
