ea50e68f80a7aed0e8b66c0c474c06d9e22a29f3
Pete <phuc.le@student.qut.edu.au>
2018-09-29 19:15:55 +1000

    create RoomMember class properties and method stubs

---
+++ mewmont/Models/RoomMember.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace mewmont.Models
+{
+    public class RoomMember
+    {
+        private Permission[] permissions;
+        private Color colour;
+        private CommMode commMode;
+
+        public Permission[] Permissions
+        {
+            get { return permissions; }
+            set { permissions = value; }
+        }
+
+        public Color Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+
+        public CommMode CommMode
+        {
+            get { return commMode; }
+            set { commMode = value; }
+        }
+
+        // Implement when message data type available
+        //public SendMessage()
+
+        public void LeaveRoom()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum CommMode { TEXT_ONLY, VIDEO };
+}
