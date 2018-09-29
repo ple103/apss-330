de94f7793784cffe08c855baaa68de5141fbc8e1
Pete <phuc.le@student.qut.edu.au>
2018-09-29 19:25:21 +1000

    create Message class and properties

---
+++ mewmont/Models/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace mewmont.Models
+{
+    public class Message
+    {
+        private string messageBody;
+        private DateTime sendTime;
+        private RoomMember author;
+
+        public string MessageBody
+        {
+            get { return messageBody; }
+            set { messageBody = value; }
+        }
+
+        public DateTime SendTime
+        {
+            get { return sendTime; }
+            set { sendTime = value; }
+        }
+
+        public RoomMember Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+    }
+}
