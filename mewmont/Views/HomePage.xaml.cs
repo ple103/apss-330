e46b1c326570225f6b01c889fa10ca4c2b8a169e
anzah <n9925147@qut.edu.au>
2018-10-11 22:00:23 +1000

    Some navigation to the Join and Create Stream from the Home Page

--- mewmont/Views/HomePage.xaml.cs
+++ mewmont/Views/HomePage.xaml.cs
@@ -11,5 +11,15 @@ namespace mewmont
         {
             InitializeComponent();
         }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new mewmont.Views.CreateStreamPage());
+        }
+
+        void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new mewmont.Views.JoinStreamPage());
+        }
     }
 }
