b3ca497c9be30a8a0a5e66bbee0751b3cae27981
Steven Harse <fukachiongakuka@gmail.com>
2018-11-01 21:26:09 +1000

    Implemented YouTube external launch

--- mewmont/Views/RoomSettingsPage.xaml.cs
+++ mewmont/Views/RoomSettingsPage.xaml.cs
@@ -17,9 +17,25 @@ namespace mewmont
 			InitializeComponent ();
 		}
 
+        /// <summary>
+        /// Change the media instantly, and return the room page upon changing media
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MediaChangeBtn_OnClick(object sender, EventArgs e)
         {
             App.RoomManager.SetRoomMedia(MediaUrlEntry.Text);
+            Navigation.PopAsync();
+        }
+
+        /// <summary>
+        /// Open the current media in the YouTube app and close the settings page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MediaExternalBtn_OnClick(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("vnd.youtube:" + App.RoomManager.Room.CurrentMedia.mediaId));
             Navigation.PopAsync();
         }
     }
